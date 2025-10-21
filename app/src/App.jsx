86c5120d149d5a7ddd305d05a330f08ade553dcf
Ayden Mathews <AYDENWM@email.sc.edu>
2025-10-21 10:37:37 -0700

    feat: version B title (intentional conflict)

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>GarnetSky – Version A</h1>
+      <h1>GarnetSky – Version B</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
