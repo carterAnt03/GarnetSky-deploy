dfdbe72ca78027ed34fb1960efb1fdf6b5fa0107
ctantley <ctantley@gmail.com>
2026-03-31 21:00:43 -0400

    fix: add crash logging and remove silent process.exit

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -25,7 +25,6 @@ pool.on('connect', () => {
 
 pool.on('error', (err) => {
   console.error('❌ Unexpected database error:', err);
-  process.exit(-1);
 });
 
 module.exports = pool;
--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,14 @@
  */
 const { createApp } = require('./app');
 
+process.on('unhandledRejection', (reason) => {
+  console.error('UNHANDLED REJECTION:', reason);
+});
+
+process.on('uncaughtException', (err) => {
+  console.error('UNCAUGHT EXCEPTION:', err);
+});
+
 const app = createApp();
 
 const PORT = process.env.PORT || 3000;
